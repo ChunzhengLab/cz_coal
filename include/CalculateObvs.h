ed393caff3a31e4c6f0b97ef79b693e5886770fa
ChunzhengLab <chunzheng.wang@icloud.com>
2024-07-10 13:08:00 +0200

    更新CalculateObvs,增加了EtaCut和PtCut

--- include/CalculateObvs.h
+++ include/CalculateObvs.h
@@ -13,49 +13,96 @@ class CalculateObvs {
   TH1D* h_pt;
   TH1D* h_eta;
   TH1D* h_phi;
+  // spectra
+  TH1D* h_pt_piLike[3];
+  TH1D* h_pt_lam[2];
+  TH1D* h_pt_pro[2];
+  // flow
+  TProfile* p_v2_pt_piLike[3];
+  TProfile* p_v2_pt_lam[2];
+  TProfile* p_v2_pt_pro[2];
   //Observable delta gamma
   TProfile* p_delta_lam_pro[4];
   TProfile* p_gamma_lam_pro[4];
-  TProfile* p_v2_pt_lam[2];
-  TProfile* p_v2_pt_pro[2];
+  //Observable C(Δφ), C(sumφ)
+  TH1D* h_dphi_lam_pro[4];
+  TH1D* h_sphi_lam_pro[4];
 
   public:
   CalculateObvs(std::string obvsFile) {
     file = std::unique_ptr<TFile>(new TFile(obvsFile.c_str(), "RECREATE"));
+
+    //QA
     h_mult = new TH1D("h_mult", "Multiplicity", 500, 0, 10000);
     h_pt = new TH1D("h_pt", "p_{T}", 100, 0, 10);
     h_eta = new TH1D("h_eta", "#eta", 100, -2.5, 2.5);
     h_phi = new TH1D("h_phi", "#phi", 100, -M_PI, M_PI);
-    // FIXME: phi range
+
+    // spectra
+    h_pt_piLike[0] = new TH1D("h_pt_piLike_0", "p_{T} #pi+ like", 200, 0, 10);
+    h_pt_piLike[1] = new TH1D("h_pt_piLike_1", "p_{T} #pi0 like", 200, 0, 10);
+    h_pt_piLike[2] = new TH1D("h_pt_piLike_2", "p_{T} #pi- like", 200, 0, 10);
+    h_pt_lam[0] = new TH1D("h_pt_lam_0", "p_{T} #Lambda", 200, 0, 10);
+    h_pt_lam[1] = new TH1D("h_pt_lam_1", "p_{T} #bar{#Lambda}", 200, 0, 10);
+    h_pt_pro[0] = new TH1D("h_pt_pro_0", "p_{T} p", 200, 0, 10);
+    h_pt_pro[1] = new TH1D("h_pt_pro_1", "p_{T} #bar{p}", 200, 0, 10);
+
+    // elliptic flow
+    p_v2_pt_piLike[0] = new TProfile("p_v2_pt_piLike_0", "v_{2} #pi+ like", 50, 0., 10.);
+    p_v2_pt_piLike[1] = new TProfile("p_v2_pt_piLike_1", "v_{2} #pi0 like", 50, 0., 10.);
+    p_v2_pt_piLike[2] = new TProfile("p_v2_pt_piLike_2", "v_{2} #pi- like", 50, 0., 10.);
+    p_v2_pt_lam[0] = new TProfile("p_v2_pt_lam_0", "v_{2} #Lambda", 50, 0., 10.);
+    p_v2_pt_lam[1] = new TProfile("p_v2_pt_lam_1", "v_{2} #bar{#Lambda}", 50, 0., 10.);
+    p_v2_pt_pro[0] = new TProfile("p_v2_pt_pro_0", "v_{2} p", 50, 0., 10.);
+    p_v2_pt_pro[1] = new TProfile("p_v2_pt_pro_1", "v_{2} #bar{p}", 50, 0., 10.);
+
+    //Observable delta gamma
     // 0 -> lambda - proton
     // 1 -> lambda - anti-proton
     // 2 -> anti-lambda - proton
     // 3 -> anti-lambda - anti-proton
     std::vector<std::string> obvsName = {"#Lambda-p", "#Lambda-#bar{p}", "#bar{#Lambda}-p", "#bar{#Lambda}-#bar{p}"};
     for (int i = 0; i < 4; i++) {
-      //FIXME
       p_delta_lam_pro[i] = new TProfile(Form("p_delta_lam_pro_%d", i), Form("#delta %s", obvsName[i].c_str()), 1, 0., 1.);
       p_gamma_lam_pro[i] = new TProfile(Form("p_gamma_lam_pro_%d", i), Form("#gamma %s", obvsName[i].c_str()), 1, 0., 1.);
     }
-    p_v2_pt_lam[0] = new TProfile("p_v2_pt_lam", "v_{2} #Lambda", 25, 0.2, 5.2);
-    p_v2_pt_lam[1] = new TProfile("p_v2_pt_pro", "v_{2} p", 25, 0.2, 5.2);
-    p_v2_pt_pro[0] = new TProfile("p_v2_pt_antilam", "v_{2} #bar{#Lambda}", 25, 0.2, 5.2);
-    p_v2_pt_pro[1] = new TProfile("p_v2_pt_antipro", "v_{2} #bar{p}", 25, 0.2, 5.2);
+    //Observable C(Δφ), C(sumφ)
+    for (int i = 0; i < 4; i++) {
+      h_dphi_lam_pro[i] = new TH1D(Form("h_dphi_lam_pro_%d", i), Form("C(#Delta#phi) %s", obvsName[i].c_str()), 100, -0.5*M_PI, 1.5*M_PI);
+      h_sphi_lam_pro[i] = new TH1D(Form("h_sphi_lam_pro_%d", i), Form("C(s#phi) %s", obvsName[i].c_str()), 100, -0.5*M_PI, 1.5*M_PI);
+    }
+  }
+
+  inline float RangeDPhi(float dphi) {
+    while (dphi < -0.5 * M_PI) dphi += 2 * M_PI;
+    while (dphi > 1.5 * M_PI) dphi -= 2 * M_PI;
+    return dphi;
   }
 
   ~CalculateObvs() {
     file->cd();
+    //QA
     h_mult->Write();
     h_pt->Write();
     h_eta->Write();
     h_phi->Write();
+    // spectra
+    for (int i = 0; i < 3; i++) h_pt_piLike[i]->Write();
+    for (int i = 0; i < 2; i++) h_pt_lam[i]->Write();
+    for (int i = 0; i < 2; i++) h_pt_pro[i]->Write();
+    // elliptic flow
+    for (int i = 0; i < 3; i++) p_v2_pt_piLike[i]->Write();
+    for (int i = 0; i < 2; i++) p_v2_pt_lam[i]->Write();
+    for (int i = 0; i < 2; i++) p_v2_pt_pro[i]->Write();
+    //Observable delta gamma
     for (int i = 0; i < 4; i++) {
       p_delta_lam_pro[i]->Write();
       p_gamma_lam_pro[i]->Write();
     }
-    for (int i = 0; i < 2; i++) {
-      p_v2_pt_lam[i]->Write();
-      p_v2_pt_pro[i]->Write();
+    //Observable C(Δφ), C(sumφ)
+    for (int i = 0; i < 4; i++) {
+      h_dphi_lam_pro[i]->Write();
+      h_sphi_lam_pro[i]->Write();
     }
     file->Close();
     // NOTE：这里需要delete吗？
--- src/CalculateObvs.cxx
+++ src/CalculateObvs.cxx
@@ -6,27 +6,52 @@ void CalculateObvs::Process(std::vector<Hadron>& hadrons) {
   h_mult->Fill(hadrons.size());
   for (int i = 0; i < hadrons.size(); i++) {
     //只要lambda 和 proton
-    h_pt->Fill(hadrons[i].Pt());
-    h_eta->Fill(hadrons[i].Eta());
-    h_phi->Fill(hadrons[i].Phi());
+    float pt = hadrons[i].Pt();
+    float eta = hadrons[i].Eta();
+    float phi = hadrons[i].Phi();
+    h_pt->Fill(pt);
+    h_eta->Fill(eta);
+    h_phi->Fill(phi);
 
-    if (hadrons[i].PDG() == 3122) {
-      p_v2_pt_lam[0]->Fill(hadrons[i].Pt(), cos(2. * hadrons[i].Phi()));
-    } else if (hadrons[i].PDG() == -3122) {
-      p_v2_pt_lam[1]->Fill(hadrons[i].Pt(), cos(2. * hadrons[i].Phi()));
-    } else if (hadrons[i].PDG() == 2212) {
-      p_v2_pt_pro[0]->Fill(hadrons[i].Pt(), cos(2. * hadrons[i].Phi()));
-    } else if (hadrons[i].PDG() == -2212) {
-      p_v2_pt_pro[1]->Fill(hadrons[i].Pt(), cos(2. * hadrons[i].Phi()));
+    if(pt < 0.2 || pt > 10.0) continue;
+    if(abs(eta) > 0.8) continue;
+
+    if (hadrons[i].PDG() == 211) {  // pi+
+      h_pt_piLike[0]->Fill(pt);
+      p_v2_pt_piLike[0]->Fill(pt, cos(2. * phi));
+    } else if (hadrons[i].PDG() == -211) { // pi-
+      h_pt_piLike[1]->Fill(pt);
+      p_v2_pt_piLike[1]->Fill(pt, cos(2. * phi));
+    } else if (hadrons[i].PDG() == 111) { // pi0
+      h_pt_piLike[2]->Fill(pt);
+      p_v2_pt_piLike[2]->Fill(pt, cos(2. * phi));
+    } else if (hadrons[i].PDG() == 3122) { // lambda
+      h_pt_lam[0]->Fill(pt);
+      p_v2_pt_lam[0]->Fill(pt, cos(2. * phi));
+    } else if (hadrons[i].PDG() == -3122) { // anti-lambda
+      h_pt_lam[1]->Fill(pt);
+      p_v2_pt_lam[1]->Fill(pt, cos(2. * phi));
+    } else if (hadrons[i].PDG() == 2212) { // proton
+      h_pt_pro[0]->Fill(pt);
+      p_v2_pt_pro[0]->Fill(pt, cos(2. * phi));
+    } else if (hadrons[i].PDG() == -2212) { // anti-proton
+      h_pt_pro[1]->Fill(pt);
+      p_v2_pt_pro[1]->Fill(pt, cos(2. * phi));
     }
 
-    if (abs(hadrons[i].PDG()) != 3122) continue;
-
+    if (abs(hadrons[i].PDG()) != 3122) continue; // 第一个粒子只选lambda(anti-lambda)
     for (int j = i + 1; j < hadrons.size(); j++) {
-      if (abs(hadrons[j].PDG()) != 2212) continue;
+      float pt_j = hadrons[j].Pt();
+      float eta_j = hadrons[j].Eta();
+      float phi_j = hadrons[j].Phi();
+
+      if (abs(hadrons[j].PDG()) != 2212) continue; // 第二个粒子只选proton(anti-proton)
+      if(pt_j < 0.2 || pt_j > 10.0) continue;
+      if(abs(eta_j) > 0.8) continue;
+
       //delta = <cos(phi_0 - phi_1)>
       //gamma = <cos(phi_0 + phi_1)>
-      float delta = cos(hadrons[i].Phi() - hadrons[j].Phi());
+      float delta = cos(phi - phi_j);
       float gamma = cos(hadrons[i].Phi() + hadrons[j].Phi());
 
       TBits bits(4);
@@ -39,6 +64,8 @@ void CalculateObvs::Process(std::vector<Hadron>& hadrons) {
         if (bits.TestBitNumber(iBit)) {
           p_delta_lam_pro[iBit]->Fill(0.5, delta);
           p_gamma_lam_pro[iBit]->Fill(0.5, gamma);
+          h_dphi_lam_pro[iBit]->Fill(RangeDPhi(hadrons[i].Phi() - hadrons[j].Phi()));
+          h_sphi_lam_pro[iBit]->Fill(RangeDPhi(hadrons[i].Phi() + hadrons[j].Phi()));
         }
       }
     }
