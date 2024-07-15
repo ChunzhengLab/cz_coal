f57d01dc8a75a6f1f77714da0471e7bd5b3957ad
ChunzhengLab <chunzheng.wang@icloud.com>
2024-07-15 15:08:00 +0200

    修正了CalculateObvs中的bug, 增加la-la对和la-pi对的计算

--- include/CalculateObvs.h
+++ include/CalculateObvs.h
@@ -4,6 +4,11 @@
 #include "TH1D.h"
 #include "TProfile.h"
 #include "Particle.h"
+#include <map>
+
+namespace par {
+  extern std::map<int, float> pdgToBinCenter;
+}
 
 class CalculateObvs {
   private:
@@ -13,20 +18,32 @@ class CalculateObvs {
   TH1D* h_pt;
   TH1D* h_eta;
   TH1D* h_phi;
+  TH1D* h_pid;
+
   // spectra
   TH1D* h_pt_piLike[3];
-  TH1D* h_pt_lam[2];
-  TH1D* h_pt_pro[2];
+  TH1D* h_pt_la[2];
+  TH1D* h_pt_pr[2];
+
   // flow
   TProfile* p_v2_pt_piLike[3];
-  TProfile* p_v2_pt_lam[2];
-  TProfile* p_v2_pt_pro[2];
+  TProfile* p_v2_pt_la[2];
+  TProfile* p_v2_pt_pr[2];
   //Observable delta gamma
-  TProfile* p_delta_lam_pro[4];
-  TProfile* p_gamma_lam_pro[4];
+  TProfile* p_delta_la_pi[4];
+  TProfile* p_delta_la_pr[4];
+  TProfile* p_delta_la_la[4];
+  TProfile* p_gamma_la_pi[4];
+  TProfile* p_gamma_la_pr[4];
+  TProfile* p_gamma_la_la[4];
+
   //Observable C(Δφ), C(sumφ)
-  TH1D* h_dphi_lam_pro[4];
-  TH1D* h_sphi_lam_pro[4];
+  TH1D* h_dphi_la_pi[4];
+  TH1D* h_dphi_la_pr[4];
+  TH1D* h_dphi_la_la[4];
+  TH1D* h_sphi_la_pi[4];
+  TH1D* h_sphi_la_pr[4];
+  TH1D* h_sphi_la_la[4];
 
   public:
   CalculateObvs(std::string obvsFile) {
@@ -37,24 +54,64 @@ class CalculateObvs {
     h_pt = new TH1D("h_pt", "p_{T}", 100, 0, 10);
     h_eta = new TH1D("h_eta", "#eta", 100, -2.5, 2.5);
     h_phi = new TH1D("h_phi", "#phi", 100, -M_PI, M_PI);
+    h_pid = new TH1D("h_pid" , "Hadron PID" , 39 , 0 , 39);
+    h_pid->GetXaxis()->SetBinLabel(1, "#pi+");
+    h_pid->GetXaxis()->SetBinLabel(2, "#pi-");
+    h_pid->GetXaxis()->SetBinLabel(3, "#pi0");
+    h_pid->GetXaxis()->SetBinLabel(4, "K+");
+    h_pid->GetXaxis()->SetBinLabel(5, "K-");
+    h_pid->GetXaxis()->SetBinLabel(6, "K0");
+    h_pid->GetXaxis()->SetBinLabel(7, "#bar{K}0");
+    h_pid->GetXaxis()->SetBinLabel(8, "#rho+");
+    h_pid->GetXaxis()->SetBinLabel(9, "#rho-");
+    h_pid->GetXaxis()->SetBinLabel(10, "#rho0");
+    h_pid->GetXaxis()->SetBinLabel(11, "#eta");
+    h_pid->GetXaxis()->SetBinLabel(12, "#omega");
+    h_pid->GetXaxis()->SetBinLabel(13, "#phi");
+    h_pid->GetXaxis()->SetBinLabel(14, "p");
+    h_pid->GetXaxis()->SetBinLabel(15, "#bar{p}");
+    h_pid->GetXaxis()->SetBinLabel(16, "n");
+    h_pid->GetXaxis()->SetBinLabel(17, "#bar{n}");
+    h_pid->GetXaxis()->SetBinLabel(18, "#Delta++");
+    h_pid->GetXaxis()->SetBinLabel(19, "#bar{#Delta}++");
+    h_pid->GetXaxis()->SetBinLabel(20, "#Delta+");
+    h_pid->GetXaxis()->SetBinLabel(21, "#bar{#Delta}+");
+    h_pid->GetXaxis()->SetBinLabel(22, "#Delta0");
+    h_pid->GetXaxis()->SetBinLabel(23, "#bar{#Delta}0");
+    h_pid->GetXaxis()->SetBinLabel(24, "#Delta-");
+    h_pid->GetXaxis()->SetBinLabel(25, "#bar{#Delta}-");
+    h_pid->GetXaxis()->SetBinLabel(26, "#Lambda");
+    h_pid->GetXaxis()->SetBinLabel(27, "#bar{#Lambda}");
+    h_pid->GetXaxis()->SetBinLabel(28, "#Sigma+");
+    h_pid->GetXaxis()->SetBinLabel(29, "#bar{#Sigma}+");
+    h_pid->GetXaxis()->SetBinLabel(30, "#Sigma0");
+    h_pid->GetXaxis()->SetBinLabel(31, "#bar{#Sigma}0");
+    h_pid->GetXaxis()->SetBinLabel(32, "#Sigma-");
+    h_pid->GetXaxis()->SetBinLabel(33, "#bar{#Sigma}-");
+    h_pid->GetXaxis()->SetBinLabel(34, "#Xi0");
+    h_pid->GetXaxis()->SetBinLabel(35, "#bar{#Xi}0");
+    h_pid->GetXaxis()->SetBinLabel(36, "#Xi-");
+    h_pid->GetXaxis()->SetBinLabel(37, "#bar{#Xi}-");
+    h_pid->GetXaxis()->SetBinLabel(38, "#Omega-");
+    h_pid->GetXaxis()->SetBinLabel(39, "#bar{#Omega}-");
 
     // spectra
     h_pt_piLike[0] = new TH1D("h_pt_piLike_0", "p_{T} #pi+ like", 200, 0, 10);
     h_pt_piLike[1] = new TH1D("h_pt_piLike_1", "p_{T} #pi0 like", 200, 0, 10);
     h_pt_piLike[2] = new TH1D("h_pt_piLike_2", "p_{T} #pi- like", 200, 0, 10);
-    h_pt_lam[0] = new TH1D("h_pt_lam_0", "p_{T} #Lambda", 200, 0, 10);
-    h_pt_lam[1] = new TH1D("h_pt_lam_1", "p_{T} #bar{#Lambda}", 200, 0, 10);
-    h_pt_pro[0] = new TH1D("h_pt_pro_0", "p_{T} p", 200, 0, 10);
-    h_pt_pro[1] = new TH1D("h_pt_pro_1", "p_{T} #bar{p}", 200, 0, 10);
+    h_pt_la[0] = new TH1D("h_pt_la_0", "p_{T} #Lambda", 200, 0, 10);
+    h_pt_la[1] = new TH1D("h_pt_la_1", "p_{T} #bar{#Lambda}", 200, 0, 10);
+    h_pt_pr[0] = new TH1D("h_pt_pr_0", "p_{T} p", 200, 0, 10);
+    h_pt_pr[1] = new TH1D("h_pt_pr_1", "p_{T} #bar{p}", 200, 0, 10);
 
     // elliptic flow
     p_v2_pt_piLike[0] = new TProfile("p_v2_pt_piLike_0", "v_{2} #pi+ like", 50, 0., 10.);
     p_v2_pt_piLike[1] = new TProfile("p_v2_pt_piLike_1", "v_{2} #pi0 like", 50, 0., 10.);
     p_v2_pt_piLike[2] = new TProfile("p_v2_pt_piLike_2", "v_{2} #pi- like", 50, 0., 10.);
-    p_v2_pt_lam[0] = new TProfile("p_v2_pt_lam_0", "v_{2} #Lambda", 50, 0., 10.);
-    p_v2_pt_lam[1] = new TProfile("p_v2_pt_lam_1", "v_{2} #bar{#Lambda}", 50, 0., 10.);
-    p_v2_pt_pro[0] = new TProfile("p_v2_pt_pro_0", "v_{2} p", 50, 0., 10.);
-    p_v2_pt_pro[1] = new TProfile("p_v2_pt_pro_1", "v_{2} #bar{p}", 50, 0., 10.);
+    p_v2_pt_la[0] = new TProfile("p_v2_pt_la_0", "v_{2} #Lambda", 50, 0., 10.);
+    p_v2_pt_la[1] = new TProfile("p_v2_pt_la_1", "v_{2} #bar{#Lambda}", 50, 0., 10.);
+    p_v2_pt_pr[0] = new TProfile("p_v2_pt_pr_0", "v_{2} p", 50, 0., 10.);
+    p_v2_pt_pr[1] = new TProfile("p_v2_pt_pr_1", "v_{2} #bar{p}", 50, 0., 10.);
 
     //Observable delta gamma
     // 0 -> lambda - proton
@@ -63,13 +120,23 @@ class CalculateObvs {
     // 3 -> anti-lambda - anti-proton
     std::vector<std::string> obvsName = {"#Lambda-p", "#Lambda-#bar{p}", "#bar{#Lambda}-p", "#bar{#Lambda}-#bar{p}"};
     for (int i = 0; i < 4; i++) {
-      p_delta_lam_pro[i] = new TProfile(Form("p_delta_lam_pro_%d", i), Form("#delta %s", obvsName[i].c_str()), 1, 0., 1.);
-      p_gamma_lam_pro[i] = new TProfile(Form("p_gamma_lam_pro_%d", i), Form("#gamma %s", obvsName[i].c_str()), 1, 0., 1.);
+      p_delta_la_pi[i] = new TProfile(Form("p_delta_la_pi_%d", i), Form("#delta %s", obvsName[i].c_str()), 1, 0., 1.);
+      p_delta_la_pr[i] = new TProfile(Form("p_delta_la_pr_%d", i), Form("#delta %s", obvsName[i].c_str()), 1, 0., 1.);
+      p_delta_la_la[i] = new TProfile(Form("p_delta_la_la_%d", i), Form("#delta %s", obvsName[i].c_str()), 1, 0., 1.);
+
+      p_gamma_la_pi[i] = new TProfile(Form("p_gamma_la_pi_%d", i), Form("#gamma %s", obvsName[i].c_str()), 1, 0., 1.);
+      p_gamma_la_pr[i] = new TProfile(Form("p_gamma_la_pr_%d", i), Form("#gamma %s", obvsName[i].c_str()), 1, 0., 1.);
+      p_gamma_la_la[i] = new TProfile(Form("p_gamma_la_la_%d", i), Form("#gamma %s", obvsName[i].c_str()), 1, 0., 1.);
     }
     //Observable C(Δφ), C(sumφ)
     for (int i = 0; i < 4; i++) {
-      h_dphi_lam_pro[i] = new TH1D(Form("h_dphi_lam_pro_%d", i), Form("C(#Delta#phi) %s", obvsName[i].c_str()), 100, -0.5*M_PI, 1.5*M_PI);
-      h_sphi_lam_pro[i] = new TH1D(Form("h_sphi_lam_pro_%d", i), Form("C(s#phi) %s", obvsName[i].c_str()), 100, -0.5*M_PI, 1.5*M_PI);
+      h_dphi_la_pi[i] = new TH1D(Form("h_dphi_la_pi_%d", i), Form("C(#Delta#phi) %s", obvsName[i].c_str()), 100, -0.5*M_PI, 1.5*M_PI);
+      h_dphi_la_pr[i] = new TH1D(Form("h_dphi_la_pr_%d", i), Form("C(#Delta#phi) %s", obvsName[i].c_str()), 100, -0.5*M_PI, 1.5*M_PI);
+      h_dphi_la_la[i] = new TH1D(Form("h_dphi_la_la_%d", i), Form("C(#Delta#phi) %s", obvsName[i].c_str()), 100, -0.5*M_PI, 1.5*M_PI);
+
+      h_sphi_la_pi[i] = new TH1D(Form("h_sphi_la_pi_%d", i), Form("C(s#phi) %s", obvsName[i].c_str()), 100, -0.5*M_PI, 1.5*M_PI);
+      h_sphi_la_pr[i] = new TH1D(Form("h_sphi_la_pr_%d", i), Form("C(s#phi) %s", obvsName[i].c_str()), 100, -0.5*M_PI, 1.5*M_PI);
+      h_sphi_la_la[i] = new TH1D(Form("h_sphi_la_la_%d", i), Form("C(s#phi) %s", obvsName[i].c_str()), 100, -0.5*M_PI, 1.5*M_PI);
     }
   }
 
@@ -88,21 +155,29 @@ class CalculateObvs {
     h_phi->Write();
     // spectra
     for (int i = 0; i < 3; i++) h_pt_piLike[i]->Write();
-    for (int i = 0; i < 2; i++) h_pt_lam[i]->Write();
-    for (int i = 0; i < 2; i++) h_pt_pro[i]->Write();
+    for (int i = 0; i < 2; i++) h_pt_la[i]->Write();
+    for (int i = 0; i < 2; i++) h_pt_pr[i]->Write();
     // elliptic flow
     for (int i = 0; i < 3; i++) p_v2_pt_piLike[i]->Write();
-    for (int i = 0; i < 2; i++) p_v2_pt_lam[i]->Write();
-    for (int i = 0; i < 2; i++) p_v2_pt_pro[i]->Write();
+    for (int i = 0; i < 2; i++) p_v2_pt_la[i]->Write();
+    for (int i = 0; i < 2; i++) p_v2_pt_pr[i]->Write();
     //Observable delta gamma
     for (int i = 0; i < 4; i++) {
-      p_delta_lam_pro[i]->Write();
-      p_gamma_lam_pro[i]->Write();
+      p_delta_la_pi[i]->Write();
+      p_delta_la_pr[i]->Write();
+      p_delta_la_la[i]->Write();
+      p_gamma_la_pi[i]->Write();
+      p_gamma_la_pr[i]->Write();
+      p_gamma_la_la[i]->Write();
     }
     //Observable C(Δφ), C(sumφ)
     for (int i = 0; i < 4; i++) {
-      h_dphi_lam_pro[i]->Write();
-      h_sphi_lam_pro[i]->Write();
+      h_dphi_la_pi[i]->Write();
+      h_dphi_la_pr[i]->Write();
+      h_dphi_la_la[i]->Write();
+      h_sphi_la_pi[i]->Write();
+      h_sphi_la_pr[i]->Write();
+      h_sphi_la_la[i]->Write();
     }
     file->Close();
     // NOTE：这里需要delete吗？
--- src/CalculateObvs.cxx
+++ src/CalculateObvs.cxx
@@ -1,6 +1,65 @@
 #include "CalculateObvs.h"
 #include "Event.h"
 #include "TBits.h"
+#include "Par.h"
+
+
+namespace par {
+  std::map<int, float> pdgToBinCenter = {
+    //pi
+    {211, 0.5},
+    {-211, 1.5},
+    {111, 2.5},
+    //K
+    {321, 3.5},
+    {-321, 4.5},
+    {311, 5.5},
+    {-311, 6.5},
+    //rho
+    {213, 7.5},
+    {-213, 8.5},
+    {113, 9.5},
+    //eta
+    {221, 10.5},
+    //omega
+    {223, 11.5},
+    //phi
+    {333, 12.5},
+    //p
+    {2212, 13.5},
+    {-2212, 14.5},
+    //n
+    {2112, 15.5},
+    {-2112, 16.5},
+    //Delta
+    {2224, 17.5},
+    {-2224, 18.5},
+    {2214, 19.5},
+    {-2214, 20.5},
+    {2114, 21.5},
+    {-2114, 22.5},
+    {1114, 23.5},
+    {-1114, 24.5},
+    //Lambda
+    {3122, 25.5},
+    {-3122, 26.5},
+    //Sigma
+    {3222, 27.5},
+    {-3222, 28.5},
+    {3212, 29.5},
+    {-3212, 30.5},
+    {3112, 31.5},
+    {-3112, 32.5},
+    //Xi
+    {3322, 33.5},
+    {-3322, 34.5},
+    {3312, 35.5},
+    {-3312, 36.5},
+    //Omega
+    {3334, 37.5},
+    {-3334, 38.5}
+  };
+}
 
 void CalculateObvs::Process(std::vector<Hadron>& hadrons) {
   h_mult->Fill(hadrons.size());
@@ -9,63 +68,95 @@ void CalculateObvs::Process(std::vector<Hadron>& hadrons) {
     float pt = hadrons[i].Pt();
     float eta = hadrons[i].Eta();
     float phi = hadrons[i].Phi();
+    int pdg = hadrons[i].PDG();
     h_pt->Fill(pt);
     h_eta->Fill(eta);
     h_phi->Fill(phi);
+    if (par::pdgToBinCenter.find(pdg) != par::pdgToBinCenter.end()) {
+      h_pid->Fill(par::pdgToBinCenter[pdg]);
+    }
 
     if(pt < 0.2 || pt > 10.0) continue;
     if(abs(eta) > 0.8) continue;
 
-    if (hadrons[i].PDG() == 211) {  // pi+
+    if (pdg == 211) {  // pi+
       h_pt_piLike[0]->Fill(pt);
       p_v2_pt_piLike[0]->Fill(pt, cos(2. * phi));
-    } else if (hadrons[i].PDG() == -211) { // pi-
+    } else if (pdg == -211) { // pi-
       h_pt_piLike[1]->Fill(pt);
       p_v2_pt_piLike[1]->Fill(pt, cos(2. * phi));
-    } else if (hadrons[i].PDG() == 111) { // pi0
+    } else if (pdg == 111) { // pi0
       h_pt_piLike[2]->Fill(pt);
       p_v2_pt_piLike[2]->Fill(pt, cos(2. * phi));
-    } else if (hadrons[i].PDG() == 3122) { // lambda
-      h_pt_lam[0]->Fill(pt);
-      p_v2_pt_lam[0]->Fill(pt, cos(2. * phi));
-    } else if (hadrons[i].PDG() == -3122) { // anti-lambda
-      h_pt_lam[1]->Fill(pt);
-      p_v2_pt_lam[1]->Fill(pt, cos(2. * phi));
-    } else if (hadrons[i].PDG() == 2212) { // proton
-      h_pt_pro[0]->Fill(pt);
-      p_v2_pt_pro[0]->Fill(pt, cos(2. * phi));
-    } else if (hadrons[i].PDG() == -2212) { // anti-proton
-      h_pt_pro[1]->Fill(pt);
-      p_v2_pt_pro[1]->Fill(pt, cos(2. * phi));
+    } else if (pdg == 3122) { // lambda
+      h_pt_la[0]->Fill(pt);
+      p_v2_pt_la[0]->Fill(pt, cos(2. * phi));
+    } else if (pdg == -3122) { // anti-lambda
+      h_pt_la[1]->Fill(pt);
+      p_v2_pt_la[1]->Fill(pt, cos(2. * phi));
+    } else if (pdg == 2112) { // proton
+      h_pt_pr[0]->Fill(pt);
+      p_v2_pt_pr[0]->Fill(pt, cos(2. * phi));
+    } else if (pdg == -2112) { // anti-proton
+      h_pt_pr[1]->Fill(pt);
+      p_v2_pt_pr[1]->Fill(pt, cos(2. * phi));
     }
 
-    if (abs(hadrons[i].PDG()) != 3122) continue; // 第一个粒子只选lambda(anti-lambda)
-    for (int j = i + 1; j < hadrons.size(); j++) {
+    if (abs(pdg) != 3122) continue; // 第一个粒子只选lambda(anti-lambda)
+
+    for (int j = 0; j < hadrons.size(); j++) {
+      if (i == j) continue;
+      if (abs(hadrons[j].PDG()) != 211 && abs(hadrons[j].PDG()) != 2112 && abs(hadrons[j].PDG()) != 3122) continue;
       float pt_j = hadrons[j].Pt();
       float eta_j = hadrons[j].Eta();
       float phi_j = hadrons[j].Phi();
-
-      if (abs(hadrons[j].PDG()) != 2212) continue; // 第二个粒子只选proton(anti-proton)
+      float pdg_j = hadrons[j].PDG();
+      // 第二个粒子只选pion(anti-pion)和proton(anti-proton)和lambda(anti-lambda)
       if(pt_j < 0.2 || pt_j > 10.0) continue;
       if(abs(eta_j) > 0.8) continue;
 
       //delta = <cos(phi_0 - phi_1)>
       //gamma = <cos(phi_0 + phi_1)>
       float delta = cos(phi - phi_j);
-      float gamma = cos(hadrons[i].Phi() + hadrons[j].Phi());
+      float gamma = cos(phi + phi_j);
 
-      TBits bits(4);
-      bits.SetBitNumber(0, hadrons[i].PDG() == 3122 && hadrons[j].PDG() == 2212);
-      bits.SetBitNumber(1, hadrons[i].PDG() == 3122 && hadrons[j].PDG() == -2212);
-      bits.SetBitNumber(2, hadrons[i].PDG() == -3122 && hadrons[j].PDG() == 2212);
-      bits.SetBitNumber(3, hadrons[i].PDG() == -3122 && hadrons[j].PDG() == -2212);
+      TBits bits_la_pi(4);
+      TBits bits_la_pr(4);
+      TBits bits_la_la(4);
+      // 0-3 -> lambda - pion
+      bits_la_pi.SetBitNumber(0, pdg == 3122 && pdg_j == 211);
+      bits_la_pi.SetBitNumber(1, pdg == 3122 && pdg_j == -211);
+      bits_la_pi.SetBitNumber(2, pdg == -3122 && pdg_j == 211);
+      bits_la_pi.SetBitNumber(3, pdg == -3122 && pdg_j == -211);
+      // 4-7 -> lambda - proton
+      bits_la_pr.SetBitNumber(0, pdg == 3122 && pdg_j == 2112);
+      bits_la_pr.SetBitNumber(1, pdg == 3122 && pdg_j == -2112);
+      bits_la_pr.SetBitNumber(2, pdg == -3122 && pdg_j == 2112);
+      bits_la_pr.SetBitNumber(3, pdg == -3122 && pdg_j == -2112);
+      // 8-11 -> lambda - lambda
+      bits_la_la.SetBitNumber(0, pdg == 3122 && pdg_j == 3122);
+      bits_la_la.SetBitNumber(1, pdg == 3122 && pdg_j == -3122);
+      bits_la_la.SetBitNumber(2, pdg == -3122 && pdg_j == 3122);
+      bits_la_la.SetBitNumber(3, pdg == -3122 && pdg_j == -3122);
 
       for (int iBit = 0; iBit < 4; iBit++) {
-        if (bits.TestBitNumber(iBit)) {
-          p_delta_lam_pro[iBit]->Fill(0.5, delta);
-          p_gamma_lam_pro[iBit]->Fill(0.5, gamma);
-          h_dphi_lam_pro[iBit]->Fill(RangeDPhi(hadrons[i].Phi() - hadrons[j].Phi()));
-          h_sphi_lam_pro[iBit]->Fill(RangeDPhi(hadrons[i].Phi() + hadrons[j].Phi()));
+        if (bits_la_pi.TestBitNumber(iBit)) {
+          p_delta_la_pi[iBit]->Fill(0.5, delta);
+          p_gamma_la_pi[iBit]->Fill(0.5, gamma);
+          h_dphi_la_pi[iBit]->Fill(RangeDPhi(phi - phi_j));
+          h_sphi_la_pi[iBit]->Fill(RangeDPhi(phi + phi_j));
+        }
+        if (bits_la_pr.TestBitNumber(iBit)) {
+          p_delta_la_pr[iBit]->Fill(0.5, delta);
+          p_gamma_la_pr[iBit]->Fill(0.5, gamma);
+          h_dphi_la_pr[iBit]->Fill(RangeDPhi(phi - phi_j));
+          h_sphi_la_pr[iBit]->Fill(RangeDPhi(phi + phi_j));
+        }
+        if (bits_la_la.TestBitNumber(iBit)) {
+          p_delta_la_la[iBit]->Fill(0.5, delta);
+          p_gamma_la_la[iBit]->Fill(0.5, gamma);
+          h_dphi_la_la[iBit]->Fill(RangeDPhi(phi - phi_j));
+          h_sphi_la_la[iBit]->Fill(RangeDPhi(phi + phi_j));
         }
       }
     }
